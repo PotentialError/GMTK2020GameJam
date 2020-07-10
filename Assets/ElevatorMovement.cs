61f913dca2c16553c65c75bd4c7439f8ae8f8ef0
Michael Rodyushkin <61915029+PotentialError@users.noreply.github.com>
2020-07-10 18:32:45 -0400

    Added Elevator with transition

---
+++ Assets/ElevatorMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorMovement : MonoBehaviour
+{
+    public float distanceUp;
+    public float speed;
+    public Vector2 target;
+    public float startTransition;
+    public GameObject transition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.up * speed * Time.deltaTime;
+        if (transform.position.y >= startTransition)
+        {
+            transition.GetComponent<SceneTransition>().LoadNextLevel(1);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+            
+        }
+    }
+}
--- Assets/SceneTransitionItems/SceneTransition.cs
+++ Assets/SceneTransitionItems/SceneTransition.cs
@@ -15,10 +15,6 @@ public class SceneTransition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            LoadNextLevel(0);
-        }
     }
     public void LoadNextLevel(int level)
     {
