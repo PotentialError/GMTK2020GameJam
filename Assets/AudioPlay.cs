955b711c12095499091c840bb0ac44d511d9ea7e
Michael Rodyushkin <61915029+PotentialError@users.noreply.github.com>
2020-07-11 12:24:48 -0400

    Added AudioCollider

---
+++ Assets/AudioPlay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlay : MonoBehaviour
+{
+    public AudioSource audio;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            StopAllAudio();
+            audio.Play();
+        }
+    }
+    void StopAllAudio()
+    {
+        AudioSource[] allAudioSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+        foreach (AudioSource audioS in allAudioSources)
+        {
+            audioS.Stop();
+        }
+    }
+}
