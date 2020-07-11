79ab76929462cdd04181bf1163c6047b35ec5474
Michael Rodyushkin <61915029+PotentialError@users.noreply.github.com>
2020-07-10 21:17:33 -0400

    Finished Elevator Movement & Add PlayerShooting

---
+++ Assets/Shoot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Transform shootPoint;
+    public GameObject bullet;
+    public Camera cam;
+    public Vector2 mousePos;
+    public bool facingRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetKeyDown("e"))
+        {
+            Shooting();
+        }
+    }
+    private void Shooting()
+    {
+        Instantiate(bullet, shootPoint.position, shootPoint.rotation);
+    }
+    private void FixedUpdate()
+    {
+        Vector2 currentPos = shootPoint.position;
+        Vector3 lookDir = mousePos - currentPos;
+        if (lookDir.x < 0f && Mathf.Abs(lookDir.x) > 1f)
+        {
+            transform.eulerAngles = new Vector3(0, -180, 0);
+            facingRight = false;
+        }
+        else if (lookDir.x > 0f && Mathf.Abs(lookDir.x) > 1f)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            facingRight = true;
+        }
+        shootPoint.eulerAngles = new Vector3(0, 0, Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg);
+    }
+}
