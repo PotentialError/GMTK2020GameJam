c6daa0c6883df6ad8c150c5105e29a67baab6822
TheSnazzmaster <43121657+TheSnazzmaster@users.noreply.github.com>
2020-07-11 17:48:30 -0700

    Revert "Revert "spawner dont spawn help""
    This reverts commit 6b7958aa03c758aeced99bab0ab380cc655b528b.

--- Assets/PatrollerSpawnerScript.cs
+++ Assets/PatrollerSpawnerScript.cs
@@ -5,16 +5,32 @@ using UnityEngine;
 public class PatrollerSpawnerScript : MonoBehaviour
 {
     public int totalSpawns;
-    private int currentSpawnNum;
+    private int currentSpawnNum = 0;
+    public GameObject thingToSpawn;
+    public float spawnCooldown;
     // Start is called before the first frame update
     void Start()
     {
         currentSpawnNum = totalSpawns;
+        StartSpawning(); //this is in case there is an audio clip that should run before it starts spawning or something
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void StartSpawning()
+    {
+        StartCoroutine(spawn());
+    }
+    IEnumerator spawn()
+    {
+        Instantiate(thingToSpawn, transform.position, thingToSpawn.transform.rotation);
+        yield return new WaitForSeconds(spawnCooldown);
+        Debug.Log("help");
+        currentSpawnNum += 1;
+        if(!(currentSpawnNum>totalSpawns))
+            StartCoroutine(spawn());
     }
 }
